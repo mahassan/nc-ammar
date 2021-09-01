ebdb5dfb4f4b03c21245b080316c898d2b306c89
Ammar Hassan <ammar@ammarhassan.co.uk>
2021-09-01 16:48:44 +0100

    added html to display page as a single article

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,7 +1,27 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, useParams } from "react-router-dom";
+import { GetTopicByID } from "../utils/apis";
 const SingleArticle = () => {
-  return <div></div>;
+  const [article, setArticle] = useState({});
+  const { article_id } = useParams();
+  useEffect(() => {
+    GetTopicByID(article_id).then(({ article }) => {
+      setArticle(article);
+    });
+  }, []);
+  return (
+    <div>
+      <header>
+        <h1>{article.title}</h1>
+        <span className="meta">By: {article.author}</span>
+        <span className="meta-count">Comments: {article.comment_count}</span>
+      </header>
+      <article>
+        <p>{article.body}</p>
+      </article>
+      <footer></footer>
+    </div>
+  );
 };
 
 export default SingleArticle;
