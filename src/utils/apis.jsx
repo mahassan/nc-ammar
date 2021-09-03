4338102b206d926bc7e2835199a36383b6b30a7e
Ammar Hassan <ammar@ammarhassan.co.uk>
2021-09-03 15:02:59 +0100

    refactored to use baseURL instead of URL again and again + added post endpoint

--- src/utils/apis.jsx
+++ src/utils/apis.jsx
@@ -1,28 +1,34 @@
+import axios from "axios";
+const baseURL = "https://ammar-evening-news.herokuapp.com/api";
 export const articlesApi = () => {
-  return fetch("https://ammar-evening-news.herokuapp.com/api/articles").then(
-    (res) => {
-      return res.json();
-    }
-  );
+  return fetch(`${baseURL}/articles`).then((res) => {
+    return res.json();
+  });
 };
 export const topicsAPI = () => {
-  return fetch("https://ammar-evening-news.herokuapp.com/api/topics").then(
-    (res) => {
-      return res.json();
-    }
-  );
+  return fetch(`${baseURL}/topics`).then((res) => {
+    return res.json();
+  });
 };
 export const GetTopicByID = (article_id) => {
-  return fetch(
-    `https://ammar-evening-news.herokuapp.com/api/articles/${article_id}`
-  ).then((res) => {
+  return fetch(`${baseURL}/articles/${article_id}`).then((res) => {
     return res.json();
   });
 };
 export const GetCommentsByID = (article_id) => {
-  return fetch(
-    `https://ammar-evening-news.herokuapp.com/api/articles/${article_id}/comments`
-  ).then((res) => {
+  return fetch(`${baseURL}/articles/${article_id}/comments`).then((res) => {
     return res.json();
   });
+};
+
+export const PostCommentByArticle = (article_id, { name, message }) => {
+  return axios
+    .post(`${baseURL}/articles/${article_id}/comments`, {
+      body: message,
+      username: name,
+    })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => console.log(err));
 };
