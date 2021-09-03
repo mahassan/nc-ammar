110074305d884a1ad5420c37cfd6831f7b881deb
Ammar Hassan <ammar@ammarhassan.co.uk>
2021-09-03 15:08:52 +0100

    moved into its own component

--- src/components/CommentsForm.jsx
+++ src/components/CommentsForm.jsx
@@ -1,14 +1,41 @@
-import React from "react";
+import { useState } from "react";
 import "../css/CommentsForm.css";
-const CommentsForm = () => {
+import { PostCommentByArticle } from "../utils/apis";
+const CommentsForm = ({ article_id }) => {
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const addComment = (event) => {
+    event.preventDefault();
+    console.log(article_id);
+    PostCommentByArticle(article_id, { name, message });
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={addComment}>
         <label htmlFor="author">Name</label>
-        <input type="text" id="author" />
+        <input
+          type="text"
+          required
+          placeholder="Enter your name"
+          id="author"
+          value={name}
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
         <label htmlFor="message">Message</label>
-        <textarea name="" id="" cols="30" rows="10"></textarea>
-        <input type="button" value="Add comment" />
+        <textarea
+          required
+          placeholder=""
+          id="message"
+          value={message}
+          cols="30"
+          rows="10"
+          onChange={(event) => {
+            setMessage(event.target.value);
+          }}
+        ></textarea>
+        <button>Submit</button>
       </form>
     </div>
   );
